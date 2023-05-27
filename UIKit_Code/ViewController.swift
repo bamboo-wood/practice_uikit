25e98be3db09ae844101e685acb8f44fa6417d32
bamboo-wood <cantate.1009.0417@gmail.com>
2023-05-27 12:14:44 +0900

    Add UILabel and UIButton

--- UIKit_Code/ViewController.swift
+++ UIKit_Code/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.text = "Hello, World!"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 24)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var button: UIButton = { [weak self] in
+        let button = UIButton(type: .system)
+        button.setTitle("Tap me", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+        view.addSubview(label)
+        view.addSubview(button)
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 20)
+        ])
     }
-
-
+    
+    @objc private func buttonTapped() {
+        label.text = "Button Tapped!!"
+    }
 }
 
